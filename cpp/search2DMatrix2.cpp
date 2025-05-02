fd41997e2645386a3424a4bd299b5ae2f8beb906
Indra Kumar <indravip3@gmail.com>
2025-05-02 14:28:05 +0530

    find Duplicate

--- cpp/search2DMatrix2.cpp
+++ cpp/search2DMatrix2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+//elena mikhailova
  bool searchMatrix(){
     vector<vector <int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
     int m = matrix.size();int n = matrix[0].size();int key = 31;
